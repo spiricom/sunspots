925048d2dc7175d8ba01ed2832fe69e419875843
Drew Wallace <drewjpwallace@gmail.com>
2016-12-20 19:47:56 -0500

    planets visual tweaks

--- planets/sketch.js
+++ planets/sketch.js
@@ -1,4 +1,6 @@
 
+// IMPORTANT NOTE: requires chrome with experimental canvas features enabled in chrome://flags (for canvas transform matrix retrieval - can be replaced with external transform system if necessary)
+
 var fps = 60;
 var centerX, centerY;
 var renderer;
@@ -49,12 +51,15 @@ function drawPlanet(planet, drawRings, drawPlanets, update) {
     if (drawRings) {
       // ring
       noFill();
+      // fill(0);
       ellipse(0, 0, p.orbitR);
 
       // ring ticks
       var numTicks = p.numTicks;
       for (var j = 0; j < numTicks; j++) {
-        var theta = map(j, 0, numTicks, 0, 360);
+        // HACK: start at >0 degrees because 0-degree subpixel line renders more opaque in chrome
+        var theta = map(j, 0, numTicks, 0.05, 360);
+        // theta += sin(theta*3) * 6;
         var v = createVector(cos(theta), sin(theta));
         var v0 = p5.Vector.mult(v, p.orbitR - p.tickInner);
         var v1 = p5.Vector.mult(v, p.orbitR + p.tickOuter);
@@ -66,12 +71,12 @@ function drawPlanet(planet, drawRings, drawPlanets, update) {
     if (drawPlanets) {
       noStroke();
       if (planet.color) fill(planet.color);
-      else              fill(131, 66, 60);
+      else              fill(200, 0, 60);
       ellipse(p.orbitR, 0, p.r);
     }
 
     // trail
-    if (drawPlanets && !p.noTrail) {
+    if (drawPlanets && !p.noTrail && (frameCount%2 === 0)) {
       var curMat = renderer.drawingContext.currentTransform;
       var invMat = curMat;
       // var invMat = curMat.inverse();
@@ -102,6 +107,9 @@ function drawPlanet(planet, drawRings, drawPlanets, update) {
 var numPlanets = 0;
 
 function draw() {
+
+  // defines the solar system
+  // refreshed every frame for live reloading purposes - can be made static without other changes
   var systemRoot = {
     color: color(255, 88, 0),
     orbitR: 0,
@@ -114,15 +122,15 @@ function draw() {
         orbitR: 0.85,
         r: 0.02,
         period: 16,
-        tickOuter: 0.03,
-        tickInner: 0.03,
+        tickOuter: 0.09,
+        tickInner: 0.19,
         numTicks: 60,
       },
       {
         orbitR: 0.35,
         r: 0.03,
         period: 8,
-        tickOuter: 0.02,
+        tickOuter: 0.07,
         tickInner: 0.02,
         numTicks: 12,
         orbiters: [
@@ -156,8 +164,11 @@ function draw() {
     ],
   };
 
+  var trailGrowRate = 0;
+  // var trailGrowRate = 0.001;
 
   background(245, 245, 243);
+  // background(245 * 0.3, 245 * 0.3, 243 * 0.3);
   t = frameCount / fps;
 
 
@@ -169,19 +180,47 @@ function draw() {
   // fill(226, 226, 224);
   // noFill();
   // noStroke();
+
+  // stroke(100);
   stroke(0);
+  // fill(245 * 0.2, 245 * 0.2, 243 * 0.2);
+  fill(245, 245, 243);
+  strokeWeight(1);
   for (var i = 0; i < trailDatas.length; i++) {
     var a = i/numTrailSegments;
     // fill(226 * ((1-a) * 0.7 + 0.3), 226 * ((1-a) * 0.7 + 0.3), 224 * ((1-a) * 0.7 + 0.3));
-    fill(255);
-    strokeWeight(1 * a);
-    ellipse(trailDatas[i].x, trailDatas[i].y, 8 * (a * 0.9 + 0.1) );
+    // strokeWeight(1 * a);
+    // ellipse(trailDatas[i].x, trailDatas[i].y, 8 * (a * 0.9 + 0.1) );
+    ellipse(trailDatas[i].x, trailDatas[i].y, 8 * (a * 0.1 + 0.9) );
+  }
+
+  if (trailGrowRate !== 0) {
+    var cx = centerX + centerX * 0.2;
+    var cy = centerY + centerY * 0.2;
+    for (var i = 0; i < trailDatas.length; i++) {
+      trailDatas[i].x = (trailDatas[i].x - cx) * (1 + trailGrowRate) + cx;
+      trailDatas[i].y = (trailDatas[i].y - cy) * (1 + trailGrowRate) + cy;
+    }
   }
 
 
-  // stroke(226 * 0.6, 226 * 0.6, 224 * 0.6);
-  stroke(226 * 0.2, 226 * 0.2, 224 * 0.2);
-  strokeWeight(1/vpR);
+  push();
+  translate(centerX, centerY);
+  translate(centerX * 0.2, centerX * 0.2);
+  scale(vpR);
+  {
+    // planets
+    drawPlanet(systemRoot, false, true);
+  }
+  pop();
+
+
+
+
+  stroke(226 * 0.6, 226 * 0.6, 224 * 0.6, 255 * 0.5);
+  // stroke(226 * 0.2, 226 * 0.2, 224 * 0.2);
+  // stroke(255);
+  strokeWeight(2/vpR);
   push();
   translate(centerX, centerY);
   translate(centerX * 0.2, centerX * 0.2);
@@ -196,24 +235,14 @@ function draw() {
     stroke(226, 226, 224);
     line(0, 0, 0, -vpR);
 
-    strokeWeight(2/vpR);
-    stroke(226 * 0.7, 226 * 0.7, 224 * 0.7);
-    line(0, 0, 0, -vpR);
-    line(0, 0, -0.1, -vpR);
+    // strokeWeight(2/vpR);
+    // stroke(226 * 0.7, 226 * 0.7, 224 * 0.7);
+    // line(0, 0, 0, -vpR);
+    // line(0, 0, -0.1, -vpR);
 
 
   }
   pop();
-
-
-  push();
-  translate(centerX, centerY);
-  translate(centerX * 0.2, centerX * 0.2);
-  scale(vpR);
-  {
-    // planets
-    drawPlanet(systemRoot, false, true);
-  }
 }
 
 
