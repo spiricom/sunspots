6710f3b2f37d280400afebf0773a89a48d5c2558
Drew Wallace <drewjpwallace@gmail.com>
2017-01-13 00:19:28 -0500

    planets - added tools for working with planet overlaps - additional update function called on overlapping pairs (onPlanetTouchingPlanetPerFrame) - events on planet-planet contact begin and end (onPlanetTouchingPlanetEnter, onPlanetTouchingPlanetExit) - planetState.numPlanetsTouching property - planetState.touchingSet: set of planets currently touching this one

--- planets/sketch.js
+++ planets/sketch.js
@@ -21,6 +21,10 @@ var howManyPlanets = 10;
 var myScale = [60, 61.05, 62.04, 62.97, 63.86, 64.71, 65.51, 67.02, 68.4, 69.05, 69.69, 70.88];
 
 function setup() {
+  // misc
+  print = console.log;
+
+  // visuals
   var viewportWidth = window.innerWidth;;
   var viewportHeight = window.innerHeight;;
   centerX = viewportWidth/2;
@@ -31,51 +35,10 @@ function setup() {
   frameRate(fps);
   ellipseMode(RADIUS);
   angleMode(DEGREES);
-  console.log("hello");
-  print = console.log;
+  
+  // audio
   initSound();
-
 }
-
-function midiToFreq(midi_code) {
-    var offset_code = midi_code - 69;
-    if (offset_code > 0) {
-        return Number(440 * Math.pow(2, offset_code / 12));
-    } else {
-        return Number(440 / Math.pow(2, -offset_code / 12));
-    }
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  var viewportWidth = window.innerWidth;
-  var viewportHeight = window.innerHeight;
-  centerX = viewportWidth/2;
-  centerY = viewportHeight/2;
-  vpR = min(centerX, centerY);
-}
-
-function ring(whichPlanet)
-{
-  for (var i = 0; i < numOsc; i++) 
-  {
-    //ramp up to the amplitude of the harmonic quickly (within 7ms)
-    gains[whichPlanet][i].gain.cancelScheduledValues(0);
-    gains[whichPlanet][i].gain.setTargetAtTime((random(0.0000001,(1.0/(numPlanets*numOsc)))), context.currentTime, 0.005);
-    //ramp down to almost zero (non-zero to avoid divide by zero in exponential function) over the decay time for the harmonic
-    gains[whichPlanet][i].gain.setTargetAtTime(0.0000001, (context.currentTime+.015), random(0.001, 1.7));
-  }
-}
-
-// gets called whenever a planet crosses the top line
-function onPlanetCrossedLine(planetName, planet) {
-  ring(planetName-1);
-  //console.log("planet crossed line: " + planetName);
-}
-
-var planetDatas = [];
-
-var trailDatas = [];
 
 function initSound(){
 
@@ -96,15 +59,12 @@ function initSound(){
     myFilter.Q.value = 1.0;
     //var fundamental = random(40, 160);
     //create a random array of frequencies
-    for (var i = 0; i < numOsc; i++) {
-      
-        randFreqs[i] = fundamental * (1);
-        print(randFreqs[i]);
+    for (var i = 0; i < numOsc; i++) { 
+      randFreqs[i] = fundamental * (1);
+      // print(randFreqs[i]);
     }
-
   
     for (var i = 0; i < numOsc; i++) {
-  
       var o = context.createOscillator();
       o.type = 'sawtooth';
       o.frequency.value = randFreqs[i];
@@ -126,6 +86,65 @@ function initSound(){
   }
 }
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  var viewportWidth = window.innerWidth;
+  var viewportHeight = window.innerHeight;
+  centerX = viewportWidth/2;
+  centerY = viewportHeight/2;
+  vpR = min(centerX, centerY);
+}
+
+
+function midiToFreq(midi_code) {
+  var offset_code = midi_code - 69;
+  if (offset_code > 0) {
+    return Number(440 * Math.pow(2, offset_code / 12));
+  } else {
+    return Number(440 / Math.pow(2, -offset_code / 12));
+  }
+}
+function ring(whichPlanet)
+{
+  for (var i = 0; i < numOsc; i++) 
+  {
+    //ramp up to the amplitude of the harmonic quickly (within 7ms)
+    gains[whichPlanet][i].gain.cancelScheduledValues(0);
+    gains[whichPlanet][i].gain.setTargetAtTime((random(0.0000001,(1.0/(numPlanets*numOsc)))), context.currentTime, 0.005);
+    //ramp down to almost zero (non-zero to avoid divide by zero in exponential function) over the decay time for the harmonic
+    gains[whichPlanet][i].gain.setTargetAtTime(0.0000001, (context.currentTime+.015), random(0.001, 1.7));
+  }
+}
+
+// gets called whenever a planet crosses the top line
+function onPlanetCrossedLine(planetName, planet) {
+  ring(planetName-1);
+  //console.log("planet crossed line: " + planetName);
+}
+
+// gets called _once_ each frame for each pair of overlapping planets
+function onPlanetTouchingPlanetPerFrame(planetName1, planet1, planetName2, planet2, centerDist) {
+  // print("planets " + planetName1 + " and " + planetName2 + " are touching with center dist " + centerDist + "!");
+}
+
+// called once whenever a pair starts touching
+function onPlanetTouchingPlanetEnter(planetName1, planet1, planetName2, planet2) {
+  print("planets " + planetName1 + " and " + planetName2 + " are now touching");
+}
+
+// called once whenever a pair stops touching
+function onPlanetTouchingPlanetExit(planetName1, planet1, planetName2, planet2) {
+  print("planets " + planetName1 + " and " + planetName2 + " are no longer touching");
+}
+
+function arePlanetsTouching(p1, p2) {
+  return p1.state.touching[p2];
+}
+
+var planetDatas = [];
+var planetsList = [];
+
+var trailDatas = [];
 
 function keyPressed() 
 {
@@ -175,7 +194,7 @@ function keyPressed()
   }
 }
 
-function drawPlanet(planet, drawRings, drawPlanets, update) {
+function updateAndDrawPlanets(planet, drawRings, drawPlanets, updatePass) {
   var p = planet;
 
   // identify planets by tree structure rather than by object reference
@@ -184,16 +203,19 @@ function drawPlanet(planet, drawRings, drawPlanets, update) {
     planetDatas[p.idx] = planetDatas[p.idx] || {};
     numPlanets++;
   }
+  planetsList[p.idx] = planet
   
-  var data = planetDatas[p.idx];
+  var planetState = planetDatas[p.idx];
+  planetState.def = planet
+  planet.state = planetState
   
   var orbitTheta = 360 * t / p.period;
 
   push();
   rotate(orbitTheta);
   {
-    // ring
-    if (drawRings) {
+    // draw ring
+    if (drawRings && p.orbitR > 0) {
       // ring
       noFill();
       // fill(0);
@@ -212,69 +234,74 @@ function drawPlanet(planet, drawRings, drawPlanets, update) {
       }
     }
 
-    // planet
+    // draw planet
     if (drawPlanets) {
       noStroke();
       if (planet.color) fill(planet.color);
       else              fill(0, 0, 0);
+      if (planet.state.numPlanetsTouching > 0) {
+        fill(255, 0, 0);
+      }
       ellipse(p.orbitR, 0, p.r);
     }
 
-    // get screen pos
-    var curMat = renderer.drawingContext.currentTransform
-    var invMat = curMat;
-    //print(invMat);
-    var x = p.orbitR;
-    var y = 0;
-    var screenPos = {
-      x: x * invMat.a + y * invMat.c + invMat.e,
-      y: x * invMat.b + y * invMat.d + invMat.f
-    };
-    
-    // arc top check (happens on drawPlanets pass)
-    if (drawPlanets && p.name !== "sun") 
-    {
-      if (typeof data.prevScreenPos === "undefined") 
-      {
-        data.prevScreenPos = screenPos;
-      } 
-      
-      if (topline)
-      {
-        if ( (screenPos.y < sunPos.y) && ((screenPos.x > sunPos.x) != (data.prevScreenPos.x > sunPos.x)) ) 
-        {
-          // crossed the line
-          onPlanetCrossedLine(p.name || p.idx, p);
-        }
+    if (updatePass) {
+      // get screen pos
+      var curMat = renderer.drawingContext.currentTransform
+      var invMat = curMat;
+      var x = p.orbitR;
+      var y = 0;
+      var screenPos = {
+        x: x * invMat.a + y * invMat.c + invMat.e,
+        y: x * invMat.b + y * invMat.d + invMat.f
+      };
+
+      // initialize prevScreenPos
+      if (typeof planetState.prevScreenPos === "undefined") {
+        planetState.prevScreenPos = screenPos;
       }
-      
-      if (bottomline)
+
+      planetState.screenPos = screenPos;
+
+      // line crossing checks
+      if (p.name !== "sun") 
       {
-        if ( (screenPos.y > sunPos.y) && ((screenPos.x > sunPos.x) != (data.prevScreenPos.x > sunPos.x)) ) 
-        {
-          onPlanetCrossedLine(p.name || p.idx, p);
-        }
-      }
-      
-      if (leftline)
-      { 
         
-        if ( (screenPos.x < sunPos.x) && ((screenPos.y > sunPos.y) != (data.prevScreenPos.y > sunPos.y)) ) 
+        if (topline)
         {
-          // crossed the line
-          onPlanetCrossedLine(p.name || p.idx, p);
+          if ( (screenPos.y < sunPos.y) && ((screenPos.x > sunPos.x) != (planetState.prevScreenPos.x > sunPos.x)) ) 
+          {
+            onPlanetCrossedLine(p.name || p.idx, p);
+          }
+        }
+        
+        if (bottomline)
+        {
+          if ( (screenPos.y > sunPos.y) && ((screenPos.x > sunPos.x) != (planetState.prevScreenPos.x > sunPos.x)) ) 
+          {
+            onPlanetCrossedLine(p.name || p.idx, p);
+          }
+        }
+        
+        if (leftline)
+        { 
+          
+          if ( (screenPos.x < sunPos.x) && ((screenPos.y > sunPos.y) != (planetState.prevScreenPos.y > sunPos.y)) ) 
+          {
+            onPlanetCrossedLine(p.name || p.idx, p);
+          }
+        }
+        
+        if (rightline)
+        {
+          if ( (screenPos.x > sunPos.x) && ((screenPos.y < sunPos.y) != (planetState.prevScreenPos.y < sunPos.y)) ) 
+          {
+            onPlanetCrossedLine(p.name || p.idx, p);
+          }
         }
       }
-      
-      if (rightline)
-      {
-        if ( (screenPos.x > sunPos.x) && ((screenPos.y < sunPos.y) != (data.prevScreenPos.y < sunPos.y)) ) 
-        {
-          // crossed the line
-          onPlanetCrossedLine(p.name || p.idx, p);
-        }
-      }
-      data.prevScreenPos = screenPos;
+
+      planetState.prevScreenPos = screenPos;
     }
 
   }
@@ -286,11 +313,73 @@ function drawPlanet(planet, drawRings, drawPlanets, update) {
     translate(cos(orbitTheta) * p.orbitR, sin(orbitTheta) * p.orbitR);
     {
       for (var i = 0; i < p.orbiters.length; i++) {
-        drawPlanet(p.orbiters[i], drawRings, drawPlanets);
+        updateAndDrawPlanets(p.orbiters[i], drawRings, drawPlanets, updatePass);
       }
     }
     pop();
   }
+}
+
+// maps planetState to set of planets with higher idx that are touching
+var prevTouchingPairs = new Map();
+var touchingPairs = new Map();
+
+function updatePlanetOverlaps() {
+
+  // refresh per-frame state
+  for (var i = 0; i < planetsList.length; i++) {
+    planetsList[i].state.touchingSet = new Set();
+    planetsList[i].state.numPlanetsTouching = 0;
+  }
+
+  prevTouchingPairs = touchingPairs;
+  touchingPairs = new Map();
+
+  for (var i = 0; i < planetsList.length-1; i++) {
+    var p1 = planetsList[i];
+    for (var j = i+1; j < planetsList.length; j++) {
+      var p2 = planetsList[j];
+      var dx = p1.state.screenPos.x - p2.state.screenPos.x;
+      var dy = p1.state.screenPos.y - p2.state.screenPos.y;
+      var dist = sqrt(dx*dx + dy*dy);
+      if (dist <= p1.r + p2.r) {
+        onPlanetTouchingPlanetPerFrame(p1.name || p1.idx, p1, p2.name || p2.idx, p2, dist);
+        
+        if (!touchingPairs.has(p1.state)) {
+          touchingPairs.set(p1.state, new Set());
+        }
+        touchingPairs.get(p1.state).add(p2.state);
+
+        p1.state.touchingSet.add(p2);
+        p2.state.touchingSet.add(p1);
+        
+        p1.state.numPlanetsTouching++;
+        p2.state.numPlanetsTouching++;
+      }
+    }
+  }
+
+  for (var state1 of prevTouchingPairs.keys()) {
+    for (var state2 of prevTouchingPairs.get(state1).keys()) {
+      if (!(touchingPairs.get(state1) && touchingPairs.get(state1).has(state2))) {
+        var p1 = state1.def;
+        var p2 = state2.def;
+        onPlanetTouchingPlanetExit(p1.name || p1.idx, p1, p2.name || p2.idx, p2);
+      }
+    }
+  }
+
+  for (var state1 of touchingPairs.keys()) {
+    for (var state2 of touchingPairs.get(state1).keys()) {
+      if (!(prevTouchingPairs.get(state1) && prevTouchingPairs.get(state1).has(state2))) {
+        var p1 = state1.def;
+        var p2 = state2.def;
+        onPlanetTouchingPlanetEnter(p1.name || p1.idx, p1, p2.name || p2.idx, p2);
+      }
+    }
+  }
+
+  prevTouchingPairs = touchingPairs;
 }
 
 var numPlanets = 0;
@@ -331,6 +420,12 @@ function draw() {
         orbitR: 0.4,
         r: 0.03,
         period: 35,
+
+      },
+      { // ADDED FOR COLLISION DEBUG
+        orbitR: 0.4,
+        r: 0.1,
+        period: 3,
 
       },
       {
@@ -384,25 +479,20 @@ function draw() {
   t = frameCount / fps;
 
 
+  // UPDATE PLANET DATA STUFF
+  updateAndDrawPlanets(systemRoot, false, false, true);
+  updatePlanetOverlaps(systemRoot);
+
 
   // trails
   var numTrailSegments = 1000;
   trailDatas.splice(0, max(0, trailDatas.length-numTrailSegments));
 
-  // fill(226, 226, 224);
-  // noFill();
-  // noStroke();
-
-  // stroke(100);
   stroke(0);
-  // fill(245 * 0.2, 245 * 0.2, 243 * 0.2);
   fill(245, 245, 243);
   strokeWeight(1);
   for (var i = 0; i < trailDatas.length; i++) {
     var a = i/numTrailSegments;
-    // fill(226 * ((1-a) * 0.7 + 0.3), 226 * ((1-a) * 0.7 + 0.3), 224 * ((1-a) * 0.7 + 0.3));
-    // strokeWeight(1 * a);
-    // ellipse(trailDatas[i].x, trailDatas[i].y, 8 * (a * 0.9 + 0.1) );
     ellipse(trailDatas[i].x, trailDatas[i].y, 8 * (a * 0.1 + 0.9) );
   }
 
@@ -416,29 +506,25 @@ function draw() {
   }
 
 
+  // DRAW PLANETS
   push();
   translate(centerX, centerY);
-  //translate(centerX * 0.2, centerX * 0.2);
   scale(vpR);
   {
-    // planets
-    drawPlanet(systemRoot, false, true);
+    updateAndDrawPlanets(systemRoot, false, true, false);
   }
   pop();
 
+  // DRAW RINGS AND LINES
   stroke(226 * 0.6, 226 * 0.6, 224 * 0.6, 255 * 0.5);
-  // stroke(226 * 0.2, 226 * 0.2, 224 * 0.2);
-  // stroke(255);
   strokeWeight(2/vpR);
   push();
   translate(centerX, centerY);
-  //translate(centerX * 0.2, centerX * 0.2);
   scale(vpR);
-  
   {
 
     // rings
-    drawPlanet(systemRoot, true, false);
+    updateAndDrawPlanets(systemRoot, true, false, false);
 
     if (topline)
     {
